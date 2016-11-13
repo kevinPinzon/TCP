bae872e3ab13d182934e56f0b44baa9dc4ad692b
kevinPinzon <alxandrp30@gmail.com>
2016-11-13 17:20:39 -0600

    muiscss agregado

---
+++ src/lenguajesRegulares/dfa.js
@@ -0,0 +1,12 @@
+
+window.onload = function() {
+  document.getElementById("btnprobarCadenaDFA").onclick = function() {myFunction()};
+  function myFunction() {
+  var cadenaEntrante = document.getElementById("inputCadena").value;
+  if (cadenaEntrante != "") {
+    
+  }else {
+    console.log("Por favor digite una cadena");
+  }
+  }
+}
