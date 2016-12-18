18451f0aec9be518e0b4de29a70a395f563b508e
kevinPinzon <alxandrp30@gmail.com>
2016-12-18 00:19:12 -0600

    guardando reglas, falta validar

--- src/CFL/cfg.js
+++ src/CFL/cfg.js
@@ -5,6 +5,8 @@ function Cfg(noTerminales,terminales,reglas,inicial) {
   this.inicial=inicial;
 
   this.setNoTerminales = function(){
+    this.reglas = new Array();
+
     var inputV = document.getElementById("inputV").value;
     var entradaIncorrecta=false;
     if (inputV != "") {
@@ -34,14 +36,61 @@ function Cfg(noTerminales,terminales,reglas,inicial) {
     if (inputE != "") {
       this.terminales = inputE.split(",");
       console.log("Variables terminales");
-      for (var i = 0; i < this.terminales.length; i++) 
+      for (var i = 0; i < this.terminales.length; i++)
         console.log(this.terminales[i]);
     }else
       alert("Por favor digite variables terminales");
   }
 
   this.setInicial = function(){
-    this.inicial = this.noTerminales[0];
+    if (this.noTerminales != null)
+      this.inicial = this.noTerminales[0];
+  }
+
+  this.setReglas = function(){
+    var reglaTemporal = new Regla("","");
+    var inputR = document.getElementById("inputR").value;
+    if (inputR != "" || inputR.length > 1) {
+      if (this.noTerminales != null || this.terminales != null || this.inicial != null) {
+        if (inputR[1] == '>') {
+          for (var i = 0; i < this.noTerminales.length; i++) {
+            if (inputR[0]==this.noTerminales[i]) {
+              reglaTemporal.inicial = inputR[0];
+            }
+          }
+        }else {
+          alert("formato de regla incorrecto, input vacio");
+        }
+      }
+      for (var i = 2; i < inputR.length; i++) {
+        for (var j = 0; j < this.noTerminales.length; j++) {
+          if (inputR[i]==this.noTerminales[j]) {
+            reglaTemporal.final += inputR[i];
+          }
+        }
+        for (var j = 0; j < this.terminales.length; j++) {
+          if (inputR[i]==this.terminales[j]) {
+            reglaTemporal.final += inputR[i];
+          }
+        }
+      }
+      console.log( reglaTemporal.toString() );
+      if ( (reglaTemporal.final != "" || reglaTemporal.final != null)
+        && (reglaTemporal.inicial != "" || reglaTemporal.inicial != null ) ) {
+        console.log( reglaTemporal.toString() );
+        this.reglas.push(reglaTemporal);
+        console.log("Reglas guardadas:");
+        for (var i = 0; i < this.reglas.length; i++) {
+          console.log( this.reglas[i].toString() );
+        }
+      }else {
+        alert("formato de regla incorrecto, no se ha guardado");
+      }
+
+    }else {
+      alert("formato de regla incorrecto, no se ha guardado");
+    }
+
   }
 
   this.getNoTerminales = function(){
@@ -52,4 +101,7 @@ function Cfg(noTerminales,terminales,reglas,inicial) {
     return this.terminales;
   }
 
+  this.getInicial = function(){
+    return this.inicial;
+  }
 }
--- src/CFL/ejecutor.js
+++ src/CFL/ejecutor.js
@@ -1,11 +1,18 @@
+var gramatica = new Cfg();
+var contador = 0;
 window.onload = function() {
-  document.getElementById("btnProbarCFG").onclick = function() {probarCFG()};
+  document.getElementById("btnRegla").onclick = function() {addRegla()};
 }
 
-function probarCFG(){
-  console.log("setiando 4tuplas");
-  var gramatica = new Cfg();
-  gramatica.setNoTerminales();
-  gramatica.setTerminales();
-  
+function addRegla(){
+  if (contador==0) {
+    console.log("setiando 4tuplas");
+    gramatica.setNoTerminales();
+    gramatica.setTerminales();
+    gramatica.setInicial();
+    document.getElementById("inputV").disabled = true;
+    document.getElementById("inputE").disabled = true;
+  }
+  gramatica.setReglas();
+  contador++;
 }
